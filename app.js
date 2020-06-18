329d9dd5d88d0a1930431096c338f130254756b6
SaraAghaAlnimer <62897692+SaraAghaAlnimer@users.noreply.github.com>
2020-06-18 17:49:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-document.getElementById("a1").addEventListener("click", function(){
-  if (user1) {
+document.getElementsByClassName("Row").addEventListener("click", function(){
       document.getElementById("a1").innerHTML = "&#11093";
-  }
-  if(user2) {
-      document.getElementById("a1").innerHTML = "&#10062";
-  }
+})
+document.getElementById("a2").addEventListener("click", function(){
+  document.getElementById("a2").innerHTML = "&#11093";
+})
+document.getElementById("a3").addEventListener("click", function(){
+  document.getElementById("a3").innerHTML = "&#11093";
+})
+//////////////
+
+
+document.getElementById("b1").addEventListener("click", function(){
+  document.getElementById("b1").innerHTML = "&#11093";
+})
+document.getElementById("b2").addEventListener("click", function(){
+  document.getElementById("b2").innerHTML = "&#11093";
+})
+document.getElementById("b3").addEventListener("click", function(){
+  document.getElementById("b3").innerHTML = "&#11093";
+})
+//////////////
+
+
+document.getElementById("c1").addEventListener("click", function(){
+  document.getElementById("c1").innerHTML = "&#11093";
+})
+document.getElementById("c2").addEventListener("click", function(){
+  document.getElementById("c2").innerHTML = "&#11093";
+})
+document.getElementById("c3").addEventListener("click", function(){
+  document.getElementById("c3").innerHTML = "&#11093";
 })
 // two players each with
 // function to pass in id argument when
